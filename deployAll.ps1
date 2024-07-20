b0707b02463b04959bccac41162742cad7ec33e5
sumanmanna134 <manna.suman134@gmail.com>
2024-07-20 12:25:14 +0530

    Add prometheus deployment- add all deployment

--- deployAll.ps1
+++ deployAll.ps1
@@ -1,2 +1,5 @@
 Write-Host "Installing RabbitMQ on cluster" -ForegroundColor Cyan
 & ((Split-Path $MyInvocation.InvocationName) + "\deployRabbitMQ.ps1")
+
+Write-Host "Installing Prometheus on cluster" -ForegroundColor Cyan
+& ((Split-Path $MyInvocation.InvocationName) + "\deployPrometheus.ps1")
